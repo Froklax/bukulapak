8aad5383e1c766e18daa811b0435af77fd32072e
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-09-16 22:42:43 +0700

    change CREF setting

--- bukulapak/settings.py
+++ bukulapak/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bukulapak.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://bertrand-gwynfory-bukulapak.pbp.cs.ui.ac.id/", "https://bertrand-gwynfory-bukulapak.pbp.cs.ui.ac.id/"]
