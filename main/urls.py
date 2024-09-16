47f2bd89285680f8f6f2c5b375b0591aeb3ff64e
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-09-16 22:47:52 +0700

    added new url paths

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_book_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-book-entry', create_book_entry, name='create_book_entry'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
 ]
+
