56d192ae6d33bf7cd339bda1d3b6bc517f57fe36
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-09-23 11:20:24 +0700

    Menghubungkan Product dengan User

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
