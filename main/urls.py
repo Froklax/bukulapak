7d83246ed167e629a9c1e6f53da65a6097444ff1
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-09-23 11:19:35 +0700

    Menambahkan routing URL

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_book_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, create_book_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
 
 app_name = 'main'
 
@@ -10,5 +10,8 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
 
