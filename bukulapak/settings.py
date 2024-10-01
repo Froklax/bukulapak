97eee41efc676563c1c07ee5e50c85099c137fbf
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-10-01 16:48:15 +0700

    modify settings

--- bukulapak/settings.py
+++ bukulapak/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q1)z5)4&dfq1rq$hgx-p0wet7^0pa79@*oylqwgx9&hmrs!e*3
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bertrand-gwynfory-bukulapak.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bertrand-gwynfory-bukulapak.pbp.cs.ui.ac.id", "192.168.18.8"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static' # merujuk ke /static root project pada mode development
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
