b5e740350dd32f4635cbbd9c17ea607c2174d806
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-10-01 16:51:50 +0700

    added url paths for new functions

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_book_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
+from main.views import show_main, create_book_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_book, delete_book
 
 app_name = 'main'
 
@@ -13,5 +13,7 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-book/<uuid:id>', edit_book, name='edit_book'),
+    path('delete/<uuid:id>', delete_book, name='delete_book'),
 ]
 
