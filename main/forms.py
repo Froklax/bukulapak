751591ada8b72a73325f643c1c26d081796fb2f6
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-10-06 22:14:28 +0700

    added clean methods

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,16 @@
 from django.forms import ModelForm
 from main.models import Product
+from django.utils.html import strip_tags
 
 class BookForm(ModelForm):
     class Meta:
         model = Product
         fields = ["name", "price", "description", "quantity"]
+
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        return strip_tags(name)
+
+    def clean_description(self):
+        description = self.cleaned_data["description"]
+        return strip_tags(description)
