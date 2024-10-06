a4b7e77b0d36c098704fde93ed3098c8e8f2f5d0
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-10-06 22:04:33 +0700

    created path /create-ajax/ for new function

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_book_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_book, delete_book
+from main.views import show_main, create_book_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_book, delete_book, add_book_entry_ajax
 
 app_name = 'main'
 
@@ -15,5 +15,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('edit-book/<uuid:id>', edit_book, name='edit_book'),
     path('delete/<uuid:id>', delete_book, name='delete_book'),
+    path('create-ajax/', add_book_entry_ajax, name='add_book_entry_ajax'),
 ]
 
