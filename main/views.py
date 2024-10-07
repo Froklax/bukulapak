20aa34a4882cf1a35a7fbfbb3ccc6a91a5490111
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-10-07 10:27:04 +0700

    added error message

--- main/views.py
+++ main/views.py
@@ -111,11 +111,13 @@ def login_user(request):
       form = AuthenticationForm(data=request.POST)
 
       if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+      else:
+        messages.error(request, "Invalid username or password. Please try again.")
 
    else:
       form = AuthenticationForm(request)
