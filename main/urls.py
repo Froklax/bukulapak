5575f7672ec6555ba1dfd3e1f1e719972b7394f8
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-11-18 14:35:23 +0700

    modif for tugas 9 flutter

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_book_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_book, delete_book, add_book_entry_ajax
+from main.views import show_main, create_book_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_book, delete_book, add_book_entry_ajax, create_book_flutter
 
 app_name = 'main'
 
@@ -16,5 +16,6 @@ urlpatterns = [
     path('edit-book/<uuid:id>', edit_book, name='edit_book'),
     path('delete/<uuid:id>', delete_book, name='delete_book'),
     path('create-ajax/', add_book_entry_ajax, name='add_book_entry_ajax'),
+    path('create-flutter/', create_book_flutter, name='create_book_flutter'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -12,6 +12,7 @@ from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
+import json
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -145,3 +146,22 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+@csrf_exempt
+def create_book_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_book = Product.objects.create(
+            user=request.user,
+            name=data["name"],
+            price=int(data["price"]),
+            description=data["description"],
+            quantity=int(data["quantity"]),
+        )
+
+        new_book.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
