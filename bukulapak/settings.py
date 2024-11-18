b3373a5bf3272ac324dced202e6cd59a4d8dcc44
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-11-18 14:34:19 +0700

    modified setting for tugas 9 flutter

--- bukulapak/settings.py
+++ bukulapak/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q1)z5)4&dfq1rq$hgx-p0wet7^0pa79@*oylqwgx9&hmrs!e*3
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bertrand-gwynfory-bukulapak.pbp.cs.ui.ac.id", "192.168.18.8"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bertrand-gwynfory-bukulapak.pbp.cs.ui.ac.id", "192.168.18.8", "10.0.2.2"]
 
 
 # Application definition
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'main',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'bukulapak.urls'
@@ -133,3 +136,10 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://bertrand-gwynfory-bukulapak.pbp.cs.ui.ac.id/", "https://bertrand-gwynfory-bukulapak.pbp.cs.ui.ac.id/"]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
